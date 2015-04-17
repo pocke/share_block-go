fdcda6f3240814ad4ccfb999a5e49f746a65371d
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-04-17 11:35:08 +0900

    Implement IDs#Diff

--- ids.go
+++ ids.go
@@ -1,7 +1,58 @@
 package main
 
+import "strconv"
+
 type IDs []int64
 
 func (s IDs) Len() int           { return len(s) }
 func (s IDs) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
 func (s IDs) Less(i, j int) bool { return s[i] < s[j] }
+
+func (l IDs) Diff(r IDs) {
+	if len(r) == 0 {
+		return
+	}
+
+	i := 0
+	for _, rv := range r {
+		for j := i; j < len(l); j++ {
+			if l[j] == rv {
+				copy(l[j:], l[j+1:])
+				l = l[:len(l)-1]
+				i = j
+				break
+			} else if l[j] > rv {
+				i = j - 1
+				break
+			}
+		}
+	}
+}
+
+func (l IDs) Eq(r IDs) bool {
+	if len(l) != len(r) {
+		return false
+	}
+	for i, lv := range l {
+		if lv != r[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func (s IDs) String() string {
+	if len(s) == 0 {
+		return "[]"
+	}
+
+	res := make([]byte, 0, len(s)*3)
+	res = append(res, '[')
+	for _, v := range s {
+		res = strconv.AppendInt(res, v, 10)
+		res = append(res, ", "...)
+	}
+	res = res[:len(res)-2]
+	res = append(res, ']')
+	return string(res)
+}
--- ids_test.go
+++ ids_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestInt64sSort(t *testing.T) {
+func TestIDsSort(t *testing.T) {
 	s := []int64{5, 3, 1, 4, 2}
 	ss := IDs(s)
 	sort.Sort(ss)
@@ -15,4 +15,37 @@ func TestInt64sSort(t *testing.T) {
 	if !reflect.DeepEqual(s, expected) {
 		t.Errorf("Expected: %q, but got: %q", expected, s)
 	}
+}
+
+func TestIDsDiff(t *testing.T) {
+	assert := func(a, b, expected []int64) {
+		l := IDs(a)
+		r := IDs(b)
+		l.Diff(r)
+		e := IDs(expected)
+		if e.Eq(r) {
+			t.Fatalf("expected: %q, but got %q", e, l)
+		}
+	}
+
+	assert(
+		[]int64{1, 2, 3, 5, 10, 11},
+		[]int64{2, 8, 11},
+		[]int64{1, 3, 5, 10},
+	)
+	assert(
+		[]int64{1, 2, 3, 4, 5},
+		[]int64{1, 2, 3, 4, 5},
+		[]int64{},
+	)
+	assert(
+		[]int64{},
+		[]int64{1, 2, 3, 4, 5},
+		[]int64{},
+	)
+	assert(
+		[]int64{1, 2, 3, 4, 5},
+		[]int64{},
+		[]int64{1, 2, 3, 4, 5},
+	)
 }
